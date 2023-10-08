96dc1c7359da9ffdfcd3b6715c20c5d13defb833
Etienne <etienne.riguet@telecom-paris.fr>
2023-10-08 16:34:51 +0200

    rl3 10 points

--- RL/rl3/student_3_2_2.py
+++ RL/rl3/student_3_2_2.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 # rewards: [golden_fish, jellyfish_1, jellyfish_2, ... , step]
-rewards = [-10, -10, -10, -10, -10, -10, -10, -10, -10, -10, -10, -10, -10]
+rewards = [20, -10, -10, -10, -10, -10, -10, -10, -10, -10, -10, -10, -1]
 
 # Q learning learning rate
-alpha = 0
+alpha = 0.5
 
 # Q learning discount rate
-gamma = 0
+gamma = 0.5
 
 # Epsilon initial
 epsilon_initial = 1
