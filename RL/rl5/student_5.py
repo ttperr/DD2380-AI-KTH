40accae33e72d3d3127c816f72746bcf0d8188c3
Tristan Perrot <tristan.perrot@telecom-paris.fr>
2023-10-09 22:27:30 +0200

    finished rl

--- RL/rl5/student_5.py
+++ RL/rl5/student_5.py
@@ -1,18 +1,18 @@
 #!/usr/bin/env python3
 # Q learning learning rate
-alpha: 0.5
+alpha = 0.5
 
 # Q learning discount rate
-gamma: 0.5
+gamma = 0.5
 
 # Epsilon initial
-epsilon_initial: 1
+epsilon_initial = 1
 
 # Epsilon final
-epsilon_final: 0.2
+epsilon_final = 0.2
 
 # Annealing timesteps
-annealing_timesteps: 10000
+annealing_timesteps = 10000
 
 # threshold
-threshold: 1e-5
+threshold = 1e-4
